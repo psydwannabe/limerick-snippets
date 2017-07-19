86d03611f7492d3b1346badf93de7501e53aa2f4
Soleil Golden <soleil@corticalstack.net>
2017-07-19 10:46:34 -0700

    The third adds a bug

--- php/ifTrue.php
+++ php/ifTrue.php
@@ -1,2 +1,12 @@
 <?php
+function isTrue($has, $three='', $args='') {
+	// the first is the only real star
+	if($has===true){
+		return($has); // to you
+	}
+	else{
+		return(false);
+	}
+	// and disbar.
+
 ?>
